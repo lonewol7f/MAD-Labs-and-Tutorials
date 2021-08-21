157c11494205a19cb11b298664c4b1ed57d6bd29
Kalana Wickramaarachchi <64887137+lonewol7f@users.noreply.github.com>
2021-08-22 00:46:22 +0530

    Additional Lab files upload

--- README.md
+++ README.md
@@ -1,5 +1,2 @@
 ## MAD LAbs and Tutorials
-### This repo use to share MAD labs and Tutorials
-
-
-* [Tutorial 4](https://github.com/lonewol7f/MAD-Labs-and-Tutorials/tree/tutorial-4)
+### Additional Lab
---
+++ app/src/main/java/com/additionallab/MainActivity.java
@@ -0,0 +1,34 @@
+package com.additionallab;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageButton btnStart = (ImageButton) findViewById(R.id.img_btn_1);
+        ImageButton btnStop = (ImageButton) findViewById(R.id.img_btn_2);
+
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startService(new Intent(MainActivity.this, MyService.class));
+            }
+        });
+
+        btnStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                stopService(new Intent(MainActivity.this, MyService.class));
+            }
+        });
+    }
+}
